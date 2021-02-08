9f5270451430016816ec98ec673bb85d1f908192
ManaQuest <72624791+ManaQuest@users.noreply.github.com>
2021-02-08 17:10:26 +0300

    Update app2.js

--- js/app2.js
+++ js/app2.js
@@ -1,6 +1,6 @@
 var p=document.querySelectorAll('.logotip .header p');
 var tr=document.querySelectorAll('.logotip .header table tr');
-var time=[[8,9,12,14],[0,55,15,10]];
+var time=[[9,11,13,15],[40,35,55,50]];
 if(localStorage.getItem('log')=='true')
 {
 	p[1].innerHTML='Добро пожаловать '+localStorage.getItem('login');
@@ -9,22 +9,15 @@ if(localStorage.getItem('log')=='true')
 var a=new Date;
 for(let i=0;i<a.getHours();i++)
 {
-	if(time[0][i]<=a.getHours()&&a.getHours()-time[0][i]==0&&tr[i])
+	if(time[0][i]==a.getHours()&&tr[i])
 	{
 		if(time[1][i]<a.getMinutes())
 		{
 			tr[i].style.backgroundColor='green';
 		}
 	}
-	else if(time[0][i]<a.getHours()&&a.getHours()-time[0][i]==1&&tr[i])
+	if(time[0][i]<a.getHours()&&tr[i])
 	{
-		if(time[1][i]<a.getMinutes())
-		{
 			tr[i].style.backgroundColor='green';
-		}
-	}
-	else if(time[0][i]<a.getHours()&&a.getHours()-time[0][i]>1&&tr[i])
-	{
-		tr[i].style.backgroundColor='green';
 	}
 }
